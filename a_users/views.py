aad4dc3914c5e07bbc69a8ea76cc9b762cae6fa5
Paul <paulcapob@gmail.com>
2024-07-27 12:52:39 -0400

    Fixed some styling and routing

--- a_users/views.py
+++ a_users/views.py
@@ -1,11 +1,10 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404, reverse
 from django.http import Http404
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib.auth import logout
 from django.contrib import messages 
 from .forms import *
-
 
 def profile_view(request, username=None):
     if username:
@@ -18,20 +17,19 @@ def profile_view(request, username=None):
 
     return render(request, 'a_users/profile.html', {'profile': profile })
 
-
 @login_required
 def profile_edit_view(request):
     form = ProfileForm(instance=request.user.profile)
-
     if request.method == 'POST':
         form = ProfileForm(request.POST, request.FILES, instance=request.user.profile)
         if form.is_valid():
             form.save()
             return redirect('profile')
-    return render(request, 'a_users/profile_edit.html', {'form': form})
-
-
-
+    if request.path == reverse('profile-onboarding'):
+        template = 'a_users/profile_onboarding.html'
+    else: 
+        template = 'a_users/profile_edit.html'
+    return render(request, template, {'form': form})
 
 @login_required
 def profile_delete_view(request):
